b2e83a7b226f8adaf4e3bd7ff390bcece98ca602
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-09 22:40:51 -0300

    Add AuthController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express'
+import { registerValidation } from '../validations/auth'
+import { createUser, generateToken, Options } from '../utils'
 
-function login(request: Request, response: Response) {}
+function login(req: Request, res: Response) {}
 
-function register(request: Request, response: Response) {
-  return response.status(201).send({ data: request.body })
+async function register(req: Request, res: Response) {
+  const { errors } = await registerValidation(req.body)
+
+  if (errors.length) return res.status(400).send({ error: errors })
+
+  const { error, user } = await createUser(req.body)
+  console.log(user)
+  if (error) return res.status(400).send({ error })
+
+  return res.status(201).send({ token: generateToken(user as Options) })
 }
 
 export default { login, register }
