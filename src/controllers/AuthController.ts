79b14128a8d714174c48544a6ed34795fa9d63c4
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-10 00:29:03 -0300

    Implement AuthController#Login

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express'
-import { registerValidation } from '../validations/auth'
+import { registerValidation, loginValidation } from '../validations/auth'
 import { createUser, generateToken, Options } from '../utils'
 
-function login(req: Request, res: Response) {}
+async function login(req: Request, res: Response) {
+  const { errors, user } = await loginValidation(req.body)
+  if (errors.length) return res.status(400).send({ error: errors })
+
+  const token = generateToken(user)
+
+  return res.send({ token })
+}
 
 async function register(req: Request, res: Response) {
   const { errors } = await registerValidation(req.body)
@@ -10,7 +17,7 @@ async function register(req: Request, res: Response) {
   if (errors.length) return res.status(400).send({ error: errors })
 
   const { error, user } = await createUser(req.body)
-  console.log(user)
+
   if (error) return res.status(400).send({ error })
 
   return res.status(201).send({ token: generateToken(user as Options) })
